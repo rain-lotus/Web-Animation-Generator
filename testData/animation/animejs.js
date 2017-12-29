9ff49a94ca3b1d1edb309ca041a709498e61913d
rain-lotus <ur4n2mk@gmail.com>
2017-12-29 14:53:46 +0900

    適当なコミット

--- testData/animation/animejs.js
+++ testData/animation/animejs.js
@@ -2,47 +2,70 @@ var target = [];
 var translatex = [];
 var translatey = [];
 
-$("#but1").click(function () {
-    target.push("#elem3");
-    translatex.push(500);
-});
+// $("#add").click(function () {
+//     var tar = $("#target").val();
+//     if (tar == "") tar = "elem";
+//     target.push("#" + tar);
+//     $("#target").val("");
+//
+//     // var trax = $("#translatex").val();
+//     // if (trax == "") trax = "250";
+//     // translatex.push(trax);
+//     // $("#translatex").val("");
+//
+//     push_val2array(translatex, "#translatex","250");
+//
+//     var tary = $("#translatey").val();
+//     if (tary == "") tary = "250";
+//     translatey.push(tary);
+//     $("#translatey").val("");
+//     $(".pala").html(target + "<br>" + translatex + "<br>" + translatey);
+// });
 
-$("#add").click(function () {
-    var tar = $("#target").val();
-    if (tar == "") tar = "elem";
-    target.push("#" + tar);
-    $("#target").val("");
-
-    // var trax = $("#translatex").val();
-    // if (trax == "") trax = "250";
-    // translatex.push(trax);
-    // $("#translatex").val("");
-
-    push_val2array(translatex, "#translatex","250");
-    var tary = $("#translatey").val();
-    if (tary == "") tary = "250";
-    translatey.push(tary);
-    $("#translatey").val("");
-    $(".pala").html(target + "<br>" + translatex + "<br>" + translatey);
-});
-
-function push_val2array(array, id, default_value) {
-    var temp = $(id).val();
-    if (temp == "") temp = array.length == 0 ? default_value : array[array.length - 1];
-    array.push(temp);
-    $(id).val("");
-}
+// function push_val2array(array, id, default_value) {
+//     var temp = $(id).val();
+//     if (temp == "") temp = array.length == 0 ? default_value : array[array.length - 1];
+//     array.push(temp);
+//     $(id).val("");
+// }
 
 $(".button").click(function () {
-    console.log(target);
-    console.log(translatex);
+    console.log("weth");
     var timeline = anime.timeline();
-    for (var i = 0; i < translatex.length; i++) {
-        timeline.add({
-            targets: target[i],
-            translateX: translatex[i],
-            translateY: translatey[i],
-            easing: 'linear'
-        });
-    }
+
+    var name = 'targets';
+
+    var animation_array = {};
+    animation_array[name] = "#elem";
+    animation_array['translateX'] = "250";
+    animation_array['easing'] = 'linear';
+
+    console.log(animation_array);
+
+    timeline.add(animation_array);
+
+    // {
+    //     targets: "#elem",
+    //         translateX: "250",
+    //     easing: "linear"
+    // }
+
+    // timeline.add({
+    //     targets: "#elem",
+    //     translateX: {
+    //         value: '+=150',
+    //         duration: 1800
+    //     },
+    //     width: {
+    //         value: '-=10',
+    //         duration: 1800,
+    //         easing: 'easeInOutSine'
+    //     },
+    //     height: {
+    //         value: '+=40',
+    //         duration: 1800,
+    //         easing: 'easeInOutSine'
+    //     },
+    //     direction: 'alternate'
+    // });
 });
