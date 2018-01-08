81b5175162c4f0e4fc5e67f523353db6bdd575ee
Der3 <tarutaru.sea@gmail.com>
2018-01-08 21:49:22 +0900

    サムネ反映

--- Definitive/post.php
+++ Definitive/post.php
@@ -3,19 +3,28 @@
 date_default_timezone_set("Asia/Tokyo");
 
 session_start();
-if (isset($_GET["html"]) && isset($_GET["animation"]) && isset($_GET["title"]) && isset($_GET["caption"])){
-    $html = $_GET["html"];
-    $animation = $_GET["animation"];
-    $title = $_GET["title"];
-    $caption = $_GET["caption"];
+if (isset($_POST["html"]) && isset($_POST["animation"]) && isset($_POST["title"]) && isset($_POST["caption"])){
+    $html = $_POST["html"];
+    $animation = $_POST["animation"];
+    $title = $_POST["title"];
+    $caption = $_POST["caption"];
     $username = $_SESSION["screen_name"];
     $time = date("Y-m-d H:i");
 
     $pdo = new PDO("sqlite:data/works.sqlite");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-    if (isset($_GET["id"])){
-        $id = $_GET["id"];
+    if (isset($_POST["thumbnail"])) {
+        $data=$_POST['thumbnail'];
+       $data = base64_decode($data);        
+       $im=imagecreatefromstring($data);
+       $timedata=date("Y-m-d H-i-s");
+        //2018-01-08 21-40-24 taizosorry.pngみたいなファイルが生成される
+       imagepng($im, "./thumbnail/".$timedata." ".$username.".png");
+        
+        }
+    if (isset($_POST["id"])){
+        $id = $_POST["id"];
 
         $st = $pdo->prepare("update sketch set username = '$username' where id = '$id'");
         $st = $pdo->prepare("update sketch set title = '$title' where id = '$id'");
@@ -23,7 +32,8 @@ if (isset($_GET["html"]) && isset($_GET["animation"]) && isset($_GET["title"]) &
         $st = $pdo->prepare("update sketch set date = '$time' where id = '$id'");
         $st = $pdo->prepare("update sketch set animation = '$animation' where id = '$id'");
         $st = $pdo->prepare("update sketch set html = '$html' where id = '$id'");
-
+        
+    
     }else{
         $st = $pdo->prepare("INSERT INTO sketch(username,title,caption,date,animation,html) VALUES( ?, ?, ?, ?, ?, ?)");
         $st->execute(array($username, $title, $caption, $time, $animation, $html));
