f5eaae22f9de2e9ccdfe98c64b6cce3be17109e6
Der3 <tarutaru.sea@gmail.com>
2018-01-08 18:58:27 +0900

    サムネイル生成js追加

---
+++ Definitive/js/makethumbnail.js
@@ -0,0 +1,13 @@
+        function screenshot(selector) {
+            var element = $(selector)[0];
+            html2canvas(element, {
+                onrendered: function(canvas) {
+                    var imgData = canvas.toDataURL();
+                     imgData = imgData.replace('data:image/png;base64,', '');  // 頭のいらない部分を落とす
+                    $('#get_thumbnail').val(imgData);
+                       $('#form').show();
+
+                }
+            });
+        }      
+
